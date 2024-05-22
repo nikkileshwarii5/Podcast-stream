38887dfbc5c31bc53c336e1b2219f0e949d816bf
DODLA NIKILESHWARI <143583926+nikkileshwarii5@users.noreply.github.com>
2024-05-22 12:14:34 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 //https://podstream.onrender.com/api
 // const API = axios.create({ baseURL: 'https://podcast.io.com/api' });
-const API = axios.create({ baseURL: `https://podstream-xg8j.onrender.com/api` }); 
+const API = axios.create({ baseURL: `https://podcast-stream.onrender.com/api` }); 
 
 //auth
 export const signIn = async ({ email, password }) => await API.post('/auth/signin', { email, password });
