97654def62056e30b7e3bd3871bd844fcec00237
DODLA NIKILESHWARI <143583926+nikkileshwarii5@users.noreply.github.com>
2024-05-29 10:44:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,39 +2,30 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import morgan from 'morgan';
 
-
-
-/**routes*/
+//routes
 import authRoutes from './routes/auth.js';
 import podcastsRoutes from './routes/podcast.js';
 import userRoutes from './routes/user.js';
 
-dotenv.config();
 const app = express();
-
+dotenv.config();
 
 /** Middlewares */
 app.use(express.json());
-
 const corsConfig = {
-    origin: process.env.REACT_APP_FRONTEND_URL || 'https://gorgeous-baklava-ba8e11.netlify.app',
-    methods: 'GET, POST, PUT, DELETE, OPTIONS', // Allow specific HTTP methods
-    allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
-   
+    origin: true,
 };
 app.use(cors(corsConfig));
-
-//cors adjustment//
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://gorgeous-baklava-ba8e11.netlify.app");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
-
+// app.use(morgan('tiny'));
+// app.disable('x-powered-by');
+// app.use(function (request, response, next) {
+//     response.header("Access-Control-Allow-Origin", "*");
+//     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 const port = process.env.PORT || 8700;
 
@@ -49,13 +40,23 @@ const connect = () => {
 
 
 app.use(express.json())
+// app.enable('trust proxy'); // optional, not needed for secure cookies
+// app.use(express.session({
+//     secret : '123456',
+//     key : 'sid',
+//     proxy : true, // add this when behind a reverse proxy, if you need secure cookies
+//     cookie : {
+//         secure : true,
+//         maxAge: 5184000000 // 2 months
+//     }
+// }));
 
-// Route middlewares
 app.use("/api/auth", authRoutes)
 app.use("/api/podcasts", podcastsRoutes)
 app.use("/api/user", userRoutes)
+// app.use("/api/project", projectRoutes)
+// app.use("/api/team", teamRoutes)
 
-//error handling middlewares
 app.use((err, req, res, next) => {
     const status = err.status || 500;
     const message = err.message || "Something went wrong";
