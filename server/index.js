8720fec4bb98b46faf08c606818233d33a73b1d6
DODLA NIKILESHWARI <143583926+nikkileshwarii5@users.noreply.github.com>
2024-05-29 10:23:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
-// import morgan from 'morgan';
 
-//routes
+
+
+/**routes*/
 import authRoutes from './routes/auth.js';
 import podcastsRoutes from './routes/podcast.js';
 import userRoutes from './routes/user.js';
@@ -15,19 +16,15 @@ const app = express();
 
 /** Middlewares */
 app.use(express.json());
+
 const corsConfig = {
+    origin: process.env.REACT_APP_FRONTEND_URL || 'https://gorgeous-baklava-ba8e11.netlify.app',
+    methods: 'GET, POST, PUT, DELETE, OPTIONS', // Allow specific HTTP methods
+    allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
-     origin: process.env.REACT_APP_FRONTEND_URL || 'https://gorgeous-baklava-ba8e11.netlify.app',
-    // origin: 'http://localhost:3000'
+   
 };
 app.use(cors(corsConfig));
-// app.use(morgan('tiny'));
-// app.disable('x-powered-by');
-// app.use(function (request, response, next) {
-//     response.header("Access-Control-Allow-Origin", "*");
-//     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
 
 //cors adjustment//
 app.use((req, res, next) => {
@@ -52,23 +49,13 @@ const connect = () => {
 
 
 app.use(express.json())
-// app.enable('trust proxy'); // optional, not needed for secure cookies
-// app.use(express.session({
-//     secret : '123456',
-//     key : 'sid',
-//     proxy : true, // add this when behind a reverse proxy, if you need secure cookies
-//     cookie : {
-//         secure : true,
-//         maxAge: 5184000000 // 2 months
-//     }
-// }));
 
+// Route middlewares
 app.use("/api/auth", authRoutes)
 app.use("/api/podcasts", podcastsRoutes)
 app.use("/api/user", userRoutes)
-// app.use("/api/project", projectRoutes)
-// app.use("/api/team", teamRoutes)
 
+//error handling middlewares
 app.use((err, req, res, next) => {
     const status = err.status || 500;
     const message = err.message || "Something went wrong";
